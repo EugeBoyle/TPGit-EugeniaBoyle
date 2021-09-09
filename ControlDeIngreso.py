9cce94dfae4251a742bcee00b82f4e41a2313478
EugeBoyle <eugeniaboyle0@gmail.com>
2021-09-09 12:35:03 -0300

    programa inconcluso

--- ControlDeIngreso.py
+++ ControlDeIngreso.py
@@ -1,7 +1,18 @@
 #Ralizar un programa que permita tener un control del ingreso
 
 
+list = []
+
+menuPrincipal = int(input("Presione una opcion\n  1_ Ingresar nuevo\n  2_ Listar\n  Opción: "))
+
+while menuPrincipal == 1:
+
+        nombre = input ("Escriba su nombre: ")
         
+        list.append(nombre) 
+        
+
+        menuPrincipal = int(input("Presione una opción\n 1_ Ingresar nuevo \n 2_ Listar\n Opción: "))   
     
         
 
